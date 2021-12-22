e853eec34395b70997c4344b694d02df4908c188
Artem F <prodevtem@gmail.com>
2021-12-22 02:38:30 +0200

    update package.json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense, lazy } from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Layout } from "./utility/context/Layout";
+import * as serviceWorker from "./serviceWorker";
+import { store } from "./redux/storeConfig/store";
+import Spinner from "./components/@vuexy/spinner/Loading-spinner";
+import Loading from "./360/Pages/Loading";
+import "./index.scss";
+import "./@fake-db";
+import UserProvider from "../src/360/context/user";
+import ProductProvider from "../src/360/context/products";
 
+const LazyApp = lazy(() => import("./App"));
+
+// configureDatabase()
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Suspense fallback={<Loading />}>
+      <ProductProvider>
+        <UserProvider>
+          <Layout>
+            <LazyApp />
+          </Layout>
+        </UserProvider>
+      </ProductProvider>
+    </Suspense>
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
